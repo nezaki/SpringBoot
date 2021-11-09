1153a5e4998bd2660473654aaa933174c649d807
nezaki <r.nezaki@gmail.com>
2021-11-09 15:56:44 +0900

    no message

--- src/main/java/nezaki/demo/application/service/ExampleService.java
+++ src/main/java/nezaki/demo/application/service/ExampleService.java
@@ -4,15 +4,15 @@ import nezaki.demo.infrastructure.entity.ExampleTable;
 import nezaki.demo.infrastructure.repository.ExampleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ExampleService {
 
-  @Autowired
-  private ExampleRepository exampleRepository;
+  @Autowired private ExampleRepository exampleRepository;
 
+  @Transactional(rollbackFor = Exception.class)
   public ExampleTable selectOne(int exampleId) {
     return exampleRepository.selectOne(exampleId);
   }
-
 }
